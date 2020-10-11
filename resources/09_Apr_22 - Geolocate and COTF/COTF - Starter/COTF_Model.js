1cd1c8a8fddb78e7a891a2969676f5be560ddf9d
Hannah Akira Jiang <61534677+hannahakirajiang@users.noreply.github.com>
2020-10-11 19:52:55 -0400

    Add files via upload

---
+++ resources/09_Apr_22 - Geolocate and COTF/COTF - Starter/COTF_Model.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+	var currentOne = "1",
+		currentBrand = "brand",
+		currentModel = "model",
+		currentPrice = "price",
+		currentPic = "pic name",
+		currentInterest = "interest",
+		currentTerm	 = "term",
+		currentSalesTax = "tax",
+		whichOne = "1";
+
+
+
---
+++ resources/09_Apr_22 - Geolocate and COTF/COTF - Starter/COTF_ViewModelStarter.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+// Model - Variable list
+var currentOne = "1",
+	currentBrand = "brand",
+	currentModel = "model",
+	currentPrice = "price",
+	currentPic = "pic name",
+	currentInterest = "interest",
+	currentTerm	 = "term",
+	currentSalesTax = "tax",
+	whichOne = "1";
+
+// initialize the page - read the XML, create the inventory list
+$(document).on("pageinit", function(){
+	
+	});
+
+// handle a click on the listview - Populate the next page with the correct information	
+$("#inventoryList").on("click","li",function(){
+	
+	});
+
+// Calculate the total payments - called by entering the #page2 or clicking #calcButton
+function calcIt(){
+
+	}
+
+$('#calcButton').click(calcIt);
+	
+});
